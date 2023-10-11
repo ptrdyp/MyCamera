4d628b119790f49bdadc55cc50fbfcf2ebe6f956
ptrdyp <alviany.prameswari@gmail.com>
2023-10-11 07:55:27 +0700

    End of "Latihan Menggunggah File ke Server dengan Multipart"

--- app/src/main/java/com/dicoding/picodiploma/mycamera/MainActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/mycamera/MainActivity.kt
@@ -79,7 +79,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun uploadImage() {
         currentImageUri?.let { uri ->
-            val imageFile = uriToFile(uri, this)
+            val imageFile = uriToFile(uri, this).reduceFileImage()
             Log.d("Image File", "showImage: ${imageFile.path}")
             val description = "Ini adalah deskripsi gambar"
 
